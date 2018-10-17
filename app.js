a3b1b17b33fafae09d5f5b0e11f234705365906b
JuneAlSi <myinboxdev@gmail.com>
2018-10-17 12:30:44 +0700

    secure authentication

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  localUserRoutes = require('./controller/localuser.routes'),
+  key = require('./key');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: key.secret }));
+
+mongoose.connect('mongodb://localhost/advancedproject6');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// localhost:3000/auth/localuser/<Routes>
+app.use('/auth/localuser', localUserRoutes);
+
+app.get('/homepage', (req, res) => {
+  // req.session.localUser
+  if (req.session.localUser) {
+    res.send(req.session.localUser);
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.listen(3000, () => console.log('listening on http://localhost:3000/'));
